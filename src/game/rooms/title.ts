228bd6be2c42b5053112b2b2fe822e091e860180
Sean Normoyle <stn825@gmail.com>
2017-08-13 12:32:33 -0400

    title screen

--- src/game/rooms/title.ts
+++ src/game/rooms/title.ts
@@ -1,11 +1,34 @@
-import { Room, Vastgame } from './../../engine/vastgame';
+import { Actor, Key, Input, Room, Sprite, Vastgame } from './../../engine/vastgame';
+import { RoomID, Settings } from './../util/enum';
+import { Registry } from './../util/registry';
 
-let blockGame = Vastgame.get();
+const TitleWidth = 310;
+const TitleHeight = 150;
 
-let titleRoom = Room.define('Title');
+let titleSprite = Sprite.define({
+    imageSource: 'resources/title.png',
+    width: TitleWidth,
+    height: TitleHeight,
+});
+
+let titleActor = Actor.define('Title', {
+    sprite: titleSprite,
+});
+
+let titleRoom = Room.define(RoomID.Title);
 titleRoom.onStart(() => {
-    console.log('hello from title room');
+    let canvasWidth = Registry.get(Settings.CanvasWidth);
+    let canvasHeight = Registry.get(Settings.CanvasHeight);
 
-    let gameRoom = Room.get('Level');
-    blockGame.setRoom(gameRoom);
+    let x = Math.floor((canvasWidth - TitleWidth) / 2);
+    let y = Math.floor((canvasHeight - TitleHeight) / 4);
+    titleRoom.createActor(titleActor, x, y);
+
+    Input.onClick(goToLevelSelect);
+    Input.onKey(Key.Any, goToLevelSelect);
+
+    function goToLevelSelect() {
+        let gameRoom = Room.get(RoomID.Level);
+        Vastgame.get().setRoom(gameRoom);
+    }
 });
